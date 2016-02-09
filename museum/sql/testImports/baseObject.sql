700ba63fdf25bb59b810f1fd89e275fd4874ef40
Kehan Harman <kehan@gaiaresources.com.au>
2016-02-09 13:56:39 +0800

    Standardise indent to 4 spaces

--- museum/sql/testImports/baseObject.sql
+++ museum/sql/testImports/baseObject.sql
@@ -1,16 +1,23 @@
 # find at least 2 representative values for each field
-(SELECT PrimaryKey_Object_Table FROM Objects WHERE ItemType IS NOT NULL LIMIT 2)
-UNION(SELECT PrimaryKey_Object_Table FROM Objects WHERE Accession_Full_ID IS NOT NULL LIMIT 2)
-UNION(SELECT PrimaryKey_Object_Table FROM Objects WHERE ItemName IS NOT NULL LIMIT 2)
-;
+(SELECT PrimaryKey_Object_Table
+ FROM Objects
+ WHERE ItemType IS NOT NULL
+ LIMIT 2)
+UNION (SELECT PrimaryKey_Object_Table
+       FROM Objects
+       WHERE Accession_Full_ID IS NOT NULL
+       LIMIT 2)
+UNION (SELECT PrimaryKey_Object_Table
+       FROM Objects
+       WHERE ItemName IS NOT NULL
+       LIMIT 2);
 ## now we've found the sample records use them as the source
 SELECT
-		Accession_Full_ID,
-		ItemName,
-		ItemType,
-		PrimaryKey_Object_Table
+    Accession_Full_ID,
+    ItemName,
+    ItemType,
+    PrimaryKey_Object_Table
 FROM
-	Objects
+    Objects
 WHERE
-	PrimaryKey_Object_Table IN(25580,25579)
-;
+    PrimaryKey_Object_Table IN (25580, 25579);
