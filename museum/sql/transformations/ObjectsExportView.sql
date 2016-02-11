e2d4e44f0d7e69692048368597d78f88ec1bd1ec
Kehan Harman <kehan@gaiaresources.com.au>
2016-02-11 09:42:56 +0800

    Sort sample data by primary key

--- museum/sql/transformations/ObjectsExportView.sql
+++ museum/sql/transformations/ObjectsExportView.sql
@@ -1,28 +1,29 @@
 CREATE OR REPLACE VIEW ObjectsExport AS
-		SELECT
-				o.*,
-				m.MethodDescription,
-				s.Address,
-				s.Deceased,
-				s.DonorNotes,
-				s.EMail,
-				s.Fax,
-				NULL           AS DonorAgentName,
-				NULL           AS DonorAgentAddress,
-				NULL           AS DonorAgentPhone,
-				s.FirstNames,
-				s.Initials,
-				s.LastEditBy   AS SourceLastEditBy,
-				s.LastEditDate AS SourceLastEditDate,
-				s.LastName,
-				s.Phone,
-				s.MobilePhone,
-				s.PostCode,
-				s.Salutation,
-				s.SourceID,
-				s.State,
-				s.TownCity
-		FROM
-				Objects o
-				LEFT JOIN Methods m ON (o.Method = m.Method)
-				LEFT JOIN Sources s ON (o.SourceName = s.SourceName);
+    SELECT
+        o.*,
+        m.MethodDescription,
+        s.Address,
+        s.Deceased,
+        s.DonorNotes,
+        s.EMail,
+        s.Fax,
+        NULL           AS DonorAgentName,
+        NULL           AS DonorAgentAddress,
+        NULL           AS DonorAgentPhone,
+        s.FirstNames,
+        s.Initials,
+        s.LastEditBy   AS SourceLastEditBy,
+        s.LastEditDate AS SourceLastEditDate,
+        s.LastName,
+        s.Phone,
+        s.MobilePhone,
+        s.PostCode,
+        s.Salutation,
+        s.SourceID,
+        s.State,
+        s.TownCity
+    FROM
+        Objects o
+        LEFT JOIN Methods m ON (o.Method = m.Method)
+        LEFT JOIN Sources s ON (o.SourceName = s.SourceName)
+    ORDER BY o.PrimaryKey_Object_Table;
