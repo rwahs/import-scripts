499d37e34b6f5d2d90751fa42a2529f39a401bbd
Kehan Harman <kehan@gaiaresources.com.au>
2016-02-26 17:25:20 +0800

    Update sample records to include objects where there are more than one history for an object
    * Include hierarchy in sample records

--- museum/sql/testImports/locationHistory.sql
+++ museum/sql/testImports/locationHistory.sql
@@ -8,8 +8,10 @@ UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE PrimaryKey_Objec
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE o_Accession_Full_ID IS NOT NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE o_ItemType IS NOT NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE topLevelLocation IS NOT NULL LIMIT 5)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE topLevelLocation IS NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE childStorageLocation IS NOT NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE childType IS NOT NULL LIMIT 5)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport GROUP BY PrimaryKey_Object_Table  HAVING count(LocationHistoryID)>1 LIMIT 5)
 
 ;
 ## now we've found the sample records use them as the source
@@ -28,5 +30,5 @@ SELECT
 FROM
     LocationHistoryExport
 WHERE
-    LocationHistoryID IN(1,2,3,4,5)
+    LocationHistoryID IN(1,2,3,4,5,41,42,43,44,45,120,164,155,221,108)
 ;
