451568c84e7ff486a9fe2fdf79835d7a59204a39
Kehan Harman <kehan@gaiaresources.com.au>
2016-03-03 11:27:59 +0800

    Add indexes to speed up condition lookups

--- museum/sql/transformations/OptimiseTables.sql
+++ museum/sql/transformations/OptimiseTables.sql
@@ -3,7 +3,11 @@ ADD PRIMARY KEY (PrimaryKey_Object_Table),
 ADD INDEX (Accession_Full_ID),
 ADD INDEX (ItemType),
 ADD INDEX (ItemName),
-ADD INDEX (SourceName);
+ADD INDEX (SourceName),
+ADD INDEX (VisualCondition),
+ADD INDEX (PhysicalCondition),
+ADD INDEX (ConditionOnReceipt);
+
 ALTER TABLE Sources
 ADD PRIMARY KEY (SourceID),
 ADD INDEX (SourceName);
@@ -13,3 +17,8 @@ ADD PRIMARY KEY (LocationHistoryID),
 ADD INDEX (PrimaryKey_Object_Table),
 ADD INDEX (LocationStatus),
 ADD INDEX (LocationDate);
+
+ALTER TABLE ReceiptConditions
+ADD PRIMARY KEY (SequenceOnForm),
+ADD INDEX (`Condition`),
+ADD INDEX (`ConditionDescription`);
