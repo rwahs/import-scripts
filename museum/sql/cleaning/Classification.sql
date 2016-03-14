b15b51c0eccb30b4c1568f67034937debc277261
Kehan Harman <kehan@gaiaresources.com.au>
2016-03-14 13:28:19 +0800

    alias object types in the output

--- museum/sql/cleaning/Classification.sql
+++ museum/sql/cleaning/Classification.sql
@@ -1,3 +1,9 @@
-SELECT PrimaryClass, SecondaryClass, TertiaryClass, COUNT(*) as NumRecords, GROUP_CONCAT(DISTINCT ItemType)
-FROM Objects o GROUP BY PrimaryClass, SecondaryClass, TertiaryClass
+SELECT
+    PrimaryClass,
+    SecondaryClass,
+    TertiaryClass,
+    COUNT(*) as NumRecords,
+    GROUP_CONCAT(DISTINCT ItemType) AS ObjectTypes
+FROM Objects o
+GROUP BY PrimaryClass, SecondaryClass, TertiaryClass
 ORDER BY PrimaryClass, SecondaryClass, TertiaryClass;
