ac5d2cc440773df7c387fba13c1f4ac4c2d39b16
Kehan Harman <kehan@gaiaresources.com.au>
2016-04-20 20:18:07 +0800

    Ad object title and image id to export view

--- importScripts/sql/transformations/ObjectImagesExportView.sql
+++ importScripts/sql/transformations/ObjectImagesExportView.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE VIEW ObjectImagesExport AS
 SELECT
     o.ItemType,
     i.Accession_Full_ID,
+    o.ItemName,
+    ObjectsImagesID,
     CONCAT_WS('/', REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ImageFilePath, '\\', '/'), 'Y:/', ''), 'Y:', ''),
                                            '//Foyer/c/Images Mosaic/', ''), '//Foyer/c/Images Mosaic/', ''),
                            '//SERVER1/Images Mosaic/', ''), ImageFileName) AS ImagePath,
