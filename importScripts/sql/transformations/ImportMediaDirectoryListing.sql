5936c2ebe246d603ccd1a01610ddbfbada8a9a9e
Kehan Harman <kehan@gaiaresources.com.au>
2016-04-21 20:54:02 +0800

    Remove blank images from directory listing

--- importScripts/sql/transformations/ImportMediaDirectoryListing.sql
+++ importScripts/sql/transformations/ImportMediaDirectoryListing.sql
@@ -3,4 +3,4 @@ CREATE TABLE MediaDirectoryListing(
 FileName VARCHAR(255) NOT NULL PRIMARY KEY );
 
 LOAD DATA LOCAL INFILE 'source/media-list.txt' INTO TABLE MediaDirectoryListing;
-
+DELETE FROM MediaDirectoryListing WHERE FileName IS NULL OR FileName = '';
