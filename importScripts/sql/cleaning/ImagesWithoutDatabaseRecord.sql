7392e8f197ada0d74d4b540cd25930e7f7789d44
Kehan Harman <kehan@gaiaresources.com.au>
2016-04-21 20:55:13 +0800

    Remove extra unused query

--- importScripts/sql/cleaning/ImagesWithoutDatabaseRecord.sql
+++ importScripts/sql/cleaning/ImagesWithoutDatabaseRecord.sql
@@ -1,2 +1,1 @@
-SELECT m.* from ObjectsImagesExport o RIGHT JOIN MediaDirectoryListing m ON (MD5(m.FileName) = MD5(o.ImagePath)) WHERE o.ObjectsImagesID is NULL ;
 SELECT m.* from MediaDirectoryListing m LEFT JOIN ObjectsImagesExport o ON (m.FileName = o.ImagePath) WHERE o.ObjectsImagesID is NULL ;
