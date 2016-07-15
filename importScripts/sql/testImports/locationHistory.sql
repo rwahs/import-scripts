f81e4442c2bab4895ff41ea9d4521dcf7332468b
Kehan Harman <kehan@gaiaresources.com.au>
2016-07-15 12:37:53 +0800

    Update location history sample data to include new data structure

--- importScripts/sql/testImports/locationHistory.sql
+++ importScripts/sql/testImports/locationHistory.sql
@@ -7,12 +7,15 @@ UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE LocationStatus I
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE PrimaryKey_Object_Table IS NOT NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE o_Accession_Full_ID IS NOT NULL LIMIT 5)
 UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE o_ItemType IS NOT NULL LIMIT 5)
-UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE topLevelLocation IS NOT NULL LIMIT 5)
-UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE topLevelLocation IS NULL LIMIT 5)
-UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE childStorageLocation IS NOT NULL LIMIT 5)
-UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE childType IS NOT NULL LIMIT 5)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE Room IS NOT NULL LIMIT 5)
 UNION(SELECT GROUP_CONCAT(LocationHistoryID) FROM LocationHistoryExport GROUP BY PrimaryKey_Object_Table  HAVING count(LocationHistoryID)>1 LIMIT 5)
-
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE ShelfDrawer IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE Shelf IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE Drawer IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE Box IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE lowestLocation IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE lowestLocationType IS NULL LIMIT 2)
+UNION(SELECT LocationHistoryID FROM LocationHistoryExport WHERE o_ItemType = 'museumartefact' IS NULL LIMIT 2)
 ;
 ## now we've found the sample records use them as the source
 SELECT
@@ -24,11 +27,17 @@ SELECT
     PrimaryKey_Object_Table,
     o_Accession_Full_ID,
     o_ItemType,
-    topLevelLocation,
-    childStorageLocation,
-    childType
+    Room,
+    ShelfDrawer,
+    Shelf,
+    Drawer,
+    Box,
+    LibraryNo,
+    OldLocation,
+    lowestLocation,
+    lowestLocationType
 FROM
     LocationHistoryExport
 WHERE
-    LocationHistoryID IN(1,2,3,4,5,41,42,43,44,45,120,121,3720,26548,164,31780,155,156,3728,31761,221,3633,108,109,3737,3744,26936)
+    LocationHistoryID IN(1,2,3,4,5,31759,3725,31758,24246,8845,8846,32962,21850,16162,8849,24493,8850,26937,3660,24198,15883,24936,21113,26026,3535,6998,170,1475,41,42,43,44,45,120,155,164,3633,3728,3737,3744,26548,26936,31761,31780)
 ;
